6d186e7044837c9a635fe7823b77ac90271e3ed4
contraflux <eg.rosenfeld@icloud.com>
2026-03-27 00:47:10 -0400

    Update visualization stub functions

--- src/visualization.jl
+++ src/visualization.jl
@@ -1,15 +1,19 @@
-function surface_2d!(args...; kwargs...)
-    error("Load GLMakie before using plot_surface!")
+function surface_2dembed!(args...; kwargs...)
+    error("Load GLMakie before using surface_2dembed!")
 end
 
-function surface_2dembed!(args...; kwargs...)
-    error("Load GLMakie before using plot_vectors!")
+function scalar_2d!(args...; kwargs...)
+    error("Load GLMakie before using scalar_2d!")
+end
+
+function scalar_2dembed!(args...; kwargs...)
+    error("Load GLMakie before using scalar_2dembed!")
 end
 
 function vectors_2d!(args...; kwargs...)
-    error("Load GLMakie before using plot_vectors!")
+    error("Load GLMakie before using vectors_2d!")
 end
 
 function vectors_2dembed!(args...; kwargs...)
-    error("Load GLMakie before using plot_vectors!")
+    error("Load GLMakie before using vectors_2dembed!")
 end
